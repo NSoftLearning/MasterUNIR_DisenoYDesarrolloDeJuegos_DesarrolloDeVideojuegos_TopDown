9a09f10e1cc83ffc65574fbc1ea3fa690bd40ab4
Nacha <i.lobossanzana@uandresbello.edu>
2026-06-20 13:07:53 -0400

    Avances Niveles + Ajustes del guardado del GameManager

---
+++ Assets/Scripts/Gameplay/Interaction/Doors/TriggerDoor.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class TriggerDoor : MonoBehaviour
+{
+    [Header("Level Transition Trigger")]
+    public bool doorInGame = false;
+    public int levelComplete;
+    public string nextSceneName;
+
+    GameManager gameManager;
+    ScenesManager scenesManager;
+    Collider2D col;
+
+    private void Start()
+    {
+        gameManager = FindAnyObjectByType<GameManager>();
+        scenesManager = FindAnyObjectByType<ScenesManager>();
+        col.enabled = true;
+
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && doorInGame)
+        {
+            col.enabled = false;
+            gameManager.CompleteLevel(levelComplete);
+            scenesManager.CallFadeOut_LoadScene(nextSceneName);
+        }
+    }
+}
+
